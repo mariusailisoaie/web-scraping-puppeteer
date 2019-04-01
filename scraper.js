1eebfa1d9df722a8c305f1989136874b8ba8d06a
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-02 00:17:00 +0200

    Get a fullhd screenshot of the page and save it

--- scraper.js
+++ scraper.js
@@ -3,8 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://addictedcoder.com/');
-  await page.screenshot({path: './screenshots/myPage.png'});
+  await page.setViewport({width: 1920, height: 1080});
+  await page.goto('https://addictedcoder.com', {waitUntil: 'networkidle2'});
+  await page.screenshot({path: './screenshots/myPage.png', fullPage: true});
 
   await browser.close();
 })();
