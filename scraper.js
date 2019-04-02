bc64aae78d6bcf07224956610bef1c73c0e64232
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-02 02:52:39 +0200

    Automate login and create new post in my react app

--- scraper.js
+++ scraper.js
@@ -1,22 +1,30 @@
-const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
   await page.setViewport({width: 1920, height: 1080});
-  await page.goto('https://github.com/mariusailisoaie?tab=repositories', {waitUntil: 'networkidle2'});
-  // await page.screenshot({path: './screenshots/myPage.png', fullPage: true});
-  // const html = await page.content();
-  await page.waitForSelector('#user-repositories-list');
-  const repoNameArray = await page.evaluate(() => {
-    const reposName = Array.from(document.querySelectorAll('#user-repositories-list ul li h3 a'));
-    return reposName.map(repoName => repoName.innerText);
-  });
-  fs.writeFile('repos.txt', repoNameArray, err => {
-    if(err) throw err;
-    console.log('Repos saved!');
-  })
+  await page.goto('https://plan-react-app.firebaseapp.com/signin', {waitUntil: 'networkidle2'});
+
+  await page.waitForSelector('form');
+  await page.focus('input[type=email]');
+  await page.keyboard.type('test@gmail.com');
+
+  await page.focus('input[type=password]');
+  await page.keyboard.type('test123');
+  await page.click('button.btn');
+  
+  await page.waitForSelector('.dashboard.container');
+  await page.click('.nav-wrapper ul li a');
+  
+  await page.waitForSelector('.container');
+  await page.focus('input[type=text]');
+  await page.keyboard.type('P');
+  
+  await page.focus('textarea');
+  await page.keyboard.type('P');
+  await page.screenshot({path: './screenshots/firebaseapp.png', fullPage: true});
+  await page.click('button.btn');
 
   await browser.close();
 })();
