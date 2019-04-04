9c3589736cb3453002c7861de09b4e0e1d75b488
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-05 00:22:11 +0200

    Calculate salary

--- browserAutomation.js
+++ browserAutomation.js
@@ -3,8 +3,8 @@ const lib = require('./screenshots/lib');
 const moment = require('moment');
 
 (async () => {
-  let browser = await puppeteer.launch({headless: false, args: ['--start-maximized']});
-  // let browser = await puppeteer.launch();
+  // let browser = await puppeteer.launch({headless: false, args: ['--start-maximized']});
+  let browser = await puppeteer.launch();
   let page = await browser.newPage();
   await page.setViewport({width: 1920, height: 1080});
   await page.goto(lib.string2, {waitUntil: 'networkidle2'});
@@ -24,24 +24,26 @@ const moment = require('moment');
 
   await page.waitForSelector('#panel-395-1-0-0 > div > ul > li:nth-child(2) > ul > li > ul > li:nth-child(28)');
   const docsNo = await page.evaluate(() => {
-    document.querySelector('#panel-395-1-0-0 > div > ul > li:nth-child(2) > ul').style.display = 'block';
-    document.querySelector('#panel-395-1-0-0 > div > ul > li:nth-child(2) > ul > li > ul').style.display = 'block';
-    return docNumber = Array.from(document.querySelectorAll('#panel-395-1-0-0 > div > ul > li:nth-child(2) > ul > li > ul')[0].children).map(li => li.innerHTML.split("'")[1]);
+    document.querySelector('#panel-395-1-0-0 > div > ul > li:nth-child(4) > ul > li > ul').style.display = 'block';
+    document.querySelector('#panel-395-1-0-0 > div > ul > li:nth-child(4) > ul > li > ul').style.display = 'block';
+    return docNumber = Array.from(document.querySelectorAll('#panel-395-1-0-0 > div > ul > li:nth-child(4) > ul > li > ul')[0].children).map(li => li.innerHTML.split("'")[1]);
   });
 
-  const minutesArray = [];
+  const salaryArray = [];
 
   for (let i = 0; i < docsNo.length; i++) {
     await page.goto(`${lib.string7}${docsNo[i]}`, {waitUntil: 'domcontentloaded'});
-    let time = await page.$eval('body > div > table > tbody > tr:nth-child(9) > td > table > tbody > tr:nth-child(2) > td:nth-child(1)', el => el.innerText);
-    time = time.split(' ')[6];
-    const timeAsMinutes = moment.duration(time, 'm').asMinutes();
-    minutesArray.push(timeAsMinutes);
+    let salary = await page.$eval('body > div > table > tbody > tr:nth-child(9) > td > table > tbody > tr:nth-child(3) > td:nth-child(5)', el => el.innerText);
+    salaryArray.push(Number(salary));
+    // time = time.split(' ')[6];
+    // const timeAsMinutes = moment.duration(time, 'm').asMinutes();
+    // minutesArray.push(timeAsMinutes);
   }
-  console.log(minutesArray);
-  console.log('minutesArray.length -> ', minutesArray.length);
-  console.log('total minutes ->', minutesArray.reduce((a, b) => a + b));
-  console.log('time as hours ->', minutesArray.reduce((a, b) => a + b) / 60);
+  console.log('salary -> ', salaryArray.reduce((a, b) => a + b));
+  // console.log(minutesArray);
+  // console.log('minutesArray.length -> ', minutesArray.length);
+  // console.log('total minutes ->', minutesArray.reduce((a, b) => a + b));
+  // console.log('time as hours ->', minutesArray.reduce((a, b) => a + b) / 60);
 
   // await page.waitFor(500);
   // await page.screenshot({path: './screenshots/omdeler.png', fullPage: true});
